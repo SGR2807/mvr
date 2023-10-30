23c94e020ee933839ae3daf2be3f26ff031f7922
SGR2807 <shaileshgrathod0812@gmail.com>
2023-10-30 16:53:23 +0530

    First Commit

---
+++ coll_check.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coll_check : MonoBehaviour
+{
+    public string scriptNameToRunOnCollision;
+    public GameObject agent = null;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Get the other object involved in the collision.
+        GameObject otherObject = collision.agent;
+
+        // Check if the other object has the same tag as this object.
+        
+        if (otherObject.tag == agent.tag)
+        {
+            // Get the script component of the new script to run.
+            spawnAgent scriptComponent = otherObject.GetComponent<spawnAgent>();
+
+            // If the script component is not null, run the script.
+            if (scriptComponent != null)
+            {
+                scriptComponent.RunScript();
+            }
+        }
+    }
+}
